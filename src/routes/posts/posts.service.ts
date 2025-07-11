7170beae8d2e99b255b59731f66018f1a1c64310
EpicVN <ledangkhoahi3@gmail.com>
2025-07-11 12:02:21 +0700

    feat: Tích hợp Prisma Service vào Shared Module

---
+++ src/routes/posts/posts.service.ts
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/shared/services/prisma.service'
+
+@Injectable()
+export class PostsService {
+  constructor(private readonly PrismaService: PrismaService) {}
+
+  getPosts() {
+    return this.PrismaService.post.findMany({})
+  }
+
+  createPost(body: any) {
+    const userId = 1;
+
+    return this.PrismaService.post.create({
+      data: {
+        title: body.title,
+        content: body.content,
+        authorId: userId,
+      },
+    })
+  }
+
+  getPost(id: string) {
+    return `Post ${id}`
+  }
+
+  updatePost(id: string, body: any) {
+    return `Updated post ${id}`
+  }
+
+  deletePost(id: string) {
+    return `Delete Post ${id}`
+  }
+}
