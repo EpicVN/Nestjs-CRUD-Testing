1d0b0a8274b5b12c9944f4f2a8397bc4f94ae78e
EpicVN <ledangkhoahi3@gmail.com>
2025-07-12 01:35:50 +0700

    feat: validate .env file

--- src/routes/posts/posts.service.ts
+++ src/routes/posts/posts.service.ts
@@ -2,6 +2,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Injectable } from '@nestjs/common'
+import envConfig from 'src/shared/services/config';
 import { PrismaService } from 'src/shared/services/prisma.service'
 
 @Injectable()
---
+++ src/shared/services/config.ts
@@ -0,0 +1,67 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
+import { plainToClass } from 'class-transformer'
+import { IsNotEmpty, IsString, validateSync } from 'class-validator'
+import fs from 'fs'
+import path from 'path'
+import { config } from 'dotenv'
+
+config({
+  path: '.env',
+})
+
+const envPath = path.resolve('.env')
+
+// Kiểm tra có file .env
+if (!fs.existsSync(envPath)) {
+  console.log('Không tìm thấy file .env')
+  process.exit(1)
+}
+
+class ConfigSchema {
+  @IsString()
+  @IsNotEmpty()
+  DATABASE_URL: string
+
+  @IsString()
+  @IsNotEmpty()
+  ACCESS_TOKEN_SECRET: string
+
+  @IsString()
+  @IsNotEmpty()
+  ACCESS_TOKEN_EXPIRES_IN: string
+
+  @IsString()
+  @IsNotEmpty()
+  REFRESH_TOKEN_SECRET: string
+
+  @IsString()
+  @IsNotEmpty()
+  REFRESH_TOKEN_EXPIRES_IN: string
+}
+
+const configServer = plainToClass(ConfigSchema, process.env, {
+  enableImplicitConversion: true, // Cho phép ép kiểu khác string ở .env
+})
+
+const error = validateSync(configServer)
+
+if (error.length > 0) {
+  console.log('Các giá trị khai báo trong file .env không hợp lệ')
+
+  const errors = error.map((errorItem) => {
+    return {
+      property: errorItem.property,
+      constraints: errorItem.constraints,
+      value: errorItem.value,
+    }
+  })
+
+  throw errors
+}
+
+const envConfig = configServer
+
+export default envConfig
