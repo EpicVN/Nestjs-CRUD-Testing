dd06c971d64469bf16eea13e7e716b76875e647f
EpicVN <ledangkhoahi3@gmail.com>
2025-07-12 17:36:34 +0700

    feat(auth): apply serialization for user response (chuẩn hóa dữ liệu trả về)

--- src/routes/auth/auth.controller.ts
+++ src/routes/auth/auth.controller.ts
@@ -1,11 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import { Body, Controller, Post, SerializeOptions } from '@nestjs/common'
+import { RegisterBodyDto, RegisterResDTO } from './auth.dto'
 import { AuthService } from './auth.service'
-import { RegisterBodyDto } from './auth.dto'
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @SerializeOptions({ type: RegisterResDTO })
   @Post('register')
   register(@Body() body: RegisterBodyDto) {
     return this.authService.register(body)
--- src/routes/auth/auth.dto.ts
+++ src/routes/auth/auth.dto.ts
@@ -1,3 +1,4 @@
+import { Exclude, Expose } from 'class-transformer'
 import { IsEmail, IsString } from 'class-validator'
 
 export class LoginBodyDTO {
@@ -10,9 +11,29 @@ export class LoginBodyDTO {
 }
 
 export class RegisterBodyDto extends LoginBodyDTO {
-  @IsString({message: 'Tên phải là chuỗi' })
+  @IsString({ message: 'Tên phải là chuỗi' })
   name: string
 
   @IsString()
   confirmPassword: string
+}
+
+export class RegisterResDTO {
+  id: number
+  email: string
+  name: string
+  createdAt: Date
+  updatedAt: Date
+
+  @Exclude()
+  password: string
+
+  @Expose()
+  get emailName() {
+    return `${this.email} - ${this.name}`
+  }
+
+  constructor(partial: Partial<RegisterResDTO>) {
+    Object.assign(this, partial)
+  }
 }
